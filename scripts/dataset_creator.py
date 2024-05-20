04dfcf56b234c95e8a4425507f9f89b922c33442
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-05-20 16:41:44 +0900

    Add scripts

--- scripts/dataset_creator.py
+++ scripts/dataset_creator.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 
+import dataclasses
+
 import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from std_srvs.srv import Trigger, TriggerResponse
+
+
+@dataclass
+class Data:
 
 
 class DatasetCreator:
     def __init__(self):
         self._create_dataset_server = rospy.Service(
             "~start_trigger", Trigger, self._handle_create_dataset
+        )
+        self._pose_sub = rospy.Subscriber(
+            "/localized_pose", PoseWithCovarianceStamped, self._pose_callback
         )
         self._start_flag = False
         self._pose = PoseWithCovarianceStamped()
@@ -17,6 +26,9 @@ class DatasetCreator:
         rospy.loginfo("Creating dataset...")
         self._start_flag = True
         return TriggerResponse(success=True, message="Dataset created")
+
+    def _pose_callback(self, msg: PoseWithCovarianceStamped):
+        self._pose = msg
 
 
 if __name__ == "__main__":
